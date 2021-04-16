82532aed139a1523071b7e96f6d8d47a5b04b6ca
jmkang <mkang@eecs.berkeley.edu>
2021-04-16 00:18:05 +0000

    pulled some chisel over

--- src/main/scala/ChiselWrapper.scala
+++ src/main/scala/ChiselWrapper.scala
@@ -13,19 +13,31 @@ class RandomBlackBox(val n: Int) extends BlackBox(Map("N" -> IntParam(n))) with 
     val in2 = Input(UInt(n.W))
     val out = Output(UInt(n.W))
   })
-  addResource("/vsrc/RealAdd.v")
+  addResource("/vsrc/Random_Temp.v")
 }
 
 class RandomHardware extends Module{
   val io = IO(new Bundle {
     val in1 = Input(UInt(64.W))
     val in2 = Input(UInt(64.W))
+    val in_valid = Input(Bool())
     val out = Output(UInt(64.W))
   })
-   val BB = Module(new RandomBlackBox(64))
-   BB.io.in1 := io.in1
-   BB.io.in1 := io.in1
-   io.out := BB.io.out
+  // Instatiate registers and Blackbox module
+  val x  = Reg(UInt())
+  val y  = Reg(UInt())
+  val BB = Module(new RandomBlackBox(64))
+
+  // Take Inputs
+  when(io.in_valid) {
+    x := io.in1
+    y := io.in2
+  }
+
+  // Wire 
+  BB.io.in1 := x
+  BB.io.in2 := y
+  io.out := BB.io.out
 }
 
 object RandomHWGenerator extends App {
