04d63fdd21dbb503276524e7d91f5d79ddfe012e
nayiri-k <nayiri@berkeley.edu>
2021-05-04 11:18:31 -0700

    Mux4

--- src/main/scala/Muxes.scala
+++ src/main/scala/Muxes.scala
@@ -12,7 +12,23 @@ class Mux2(val n:Int) extends Module {
   })
   val sel = io.in(2*n).asBool
   val in1 = io.in(2*n-1,n)
-  val in2 = io.in(n-1,0)
+  val in0 = io.in(n-1,0)
   when(sel)   { io.out := in1 }
-  .otherwise  { io.out := in2 }
+  .otherwise  { io.out := in0 }
+}
+
+class Mux4(val n:Int) extends Module {
+  val io = IO(new Bundle {
+    val in  = Input(UInt((4*n+2).W))
+    val out  = Output(UInt(n.W))
+  })
+  val sel = io.in(4*n+1,4*n).asBool
+  val in3 = io.in(4*n-1,3*n)
+  val in2 = io.in(3*n-1,2*n)
+  val in1 = io.in(2*n-1,n)
+  val in0 = io.in(n-1,0)
+  when     (sel == "11")  { io.out := in3 }
+  .elsewhen(sel == "10")  { io.out := in2 }
+  .elsewhen(sel == "01")  { io.out := in1 }
+  .otherwise              { io.out := in0 }
 }
